a1e06c6c394f96eeb33e4b183971a3d5c534838d
Deep Velani <deepvelani@gmail.com>
2021-06-24 09:20:43 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
+        ullam nostrum inventore, doloribus, quasi id deserunt natus reiciendis
+        provident temporibus quas dignissimos eius optio alias voluptatibus
+        culpa, fugiat asperiores. Officia.
+      </p>
+    </section>
+  )
+}
+
+export default About
